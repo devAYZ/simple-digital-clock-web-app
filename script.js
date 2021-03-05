00a04bc4240eb5394f4ee6d86a9f7f8dfda05bcd
devAYZ <ayokunle.fatokimi@decagon.dev>
2021-03-05 15:59:59 +0100

    add a constant to get minutes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const print = document.querySelector('.show');
 nowTime = () => {
     const now = new Date();
     const h = now.getHours();
+    const m = now.getMinutes();
 
 }
 setInterval(nowTime, 1000);
